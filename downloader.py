7738c96af8294d60d7920f1afa5f45e07c9d260f
Justin Li <justin355li@gmail.com>
2023-06-22 01:40:14 -0400

    commit

--- downloader.py
+++ downloader.py
@@ -3,4 +3,8 @@ from pytube import YouTube
 url = "https://www.youtube.com/watch?v=3QhAm5aFEt8"
 vid = YouTube(url)
 
-print(vid.title)
+# these three lines below all give me errors idk why
+print(vid.title) # output vid name
+
+# vid = vid.streams.get_highest_resolution()
+# vid.download()
